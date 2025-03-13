57b54b16010157a3d6a77f7fb682ef3529c37a19
DiriRH19 <rdirison@gmail.com>
2025-03-13 17:29:04 -0500

    Terminado-Rozoo

--- src/components/layouts/Header/Header.jsx
+++ src/components/layouts/Header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
 
   return (
     <header className="fixed top-0 left-0 w-full bg-purple-300 text-white p-4 shadow-md z-50">
-      <div className="flex justify-between items-center max-w-screen-xl mx-auto h-20 w-80">
-        <h1 className=" font-bold">Mi Portafolio</h1>
+      <div className="flex justify-between items-center max-w-screen-lg mx-auto h-18 w-full ">
+        <h1 className="text-xl font-bold mx-auto">Mi Portafolio</h1>
 
         <button 
           className="md:hidden bg-purple-500 text-white p-2 rounded-md" 
@@ -17,21 +17,21 @@ const Header = () => {
         >
           {menuOpen ? <X size={30} /> : <Menu size={30} />}
         </button>
-
-        <nav className={`absolute md:static top-16 left-0 w-full bg-gray-300 md:bg-transparent transition-all 
-          ${menuOpen ? 'block' : 'hidden'} md:flex md:space-x-6 text-center`}
-        >
-          <ul className="flex flex-col md:flex-row md:space-x-6">
-            <li><Link to="/" className="block py-2 px-4 hover:bg-white-700">Inicio</Link></li>
-            <li><Link to="/habilidades" className="block py-2 px-4 hover:bg-white-700">Habilidades Técnicas</Link></li>
-            <li><Link to="/sociales" className="block py-2 px-4 hover:bg-white-700">Habilidades Sociales</Link></li>
-            <li><Link to="/ingles" className="block py-2 px-4 hover:bg-white-700">Nivel de Inglés</Link></li>
-            <li><Link to="/experiencia" className="block py-2 px-4 hover:bg-white-700">Experiencia</Link></li>
-            <li><Link to="/estudios" className="block py-2 px-4 hover:bg-white-700">Estudios</Link></li>
-            <li><Link to="/redes" className="block py-2 px-4 hover:bg-white-700">Redes Sociales</Link></li>
-          </ul>
-        </nav>
       </div>
+
+      <nav className={`absolute md:static top-16 left-0 w-full bg-gray-300 md:bg-transparent transition-all 
+        ${menuOpen ? 'block' : 'hidden'} md:flex md:justify-center text-center`}
+      >
+        <ul className="flex flex-col md:flex-row md:space-x-6">
+          <li><Link to="/" className="block py-2 px-4 hover:bg-white-700">Inicio</Link></li>
+          <li><Link to="/habilidades" className="block py-2 px-4 hover:bg-white-700">Habilidades Técnicas</Link></li>
+          <li><Link to="/sociales" className="block py-2 px-4 hover:bg-white-700">Habilidades Sociales</Link></li>
+          <li><Link to="/ingles" className="block py-2 px-4 hover:bg-white-700">Nivel de Inglés</Link></li>
+          <li><Link to="/experiencia" className="block py-2 px-4 hover:bg-white-700">Experiencia</Link></li>
+          <li><Link to="/estudios" className="block py-2 px-4 hover:bg-white-700">Estudios</Link></li>
+          <li><Link to="/redes" className="block py-2 px-4 hover:bg-white-700">Redes Sociales</Link></li>
+        </ul>
+      </nav>
     </header>
   );
 };
